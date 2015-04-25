c8f3dfe050f0c9ee24deedb837879b781b15f147
Tiago Vinícius de Oliveira Brachini <tvobrachini@gmail.com>
2015-04-25 15:53:43 -0300

    Sending template files with twitter-bootstrap

--- minhas_tarefas/tarefas/views/tarefas.py
+++ minhas_tarefas/tarefas/views/tarefas.py
@@ -1,6 +1,7 @@
 from django.views.generic import View
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
+from tarefas.models import Tarefa
 
 
 class Home(View):
@@ -8,6 +9,8 @@ class Home(View):
     context = {}
 
     def get(self, request, *args, **kwargs):
+        self.context['counter'] = Tarefa.objects.filter(
+            finalizado=False).count()
         return render_to_response(self.template_name, self.context,
                                   RequestContext(request))
 
