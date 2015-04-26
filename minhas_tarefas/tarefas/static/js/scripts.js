c9be6ea6b9c4b9f28669681e0cae911147cd31b2
Tiago Vinícius de Oliveira Brachini <tvobrachini@gmail.com>
2015-04-26 14:32:47 -0300

    Sending static files

---
+++ minhas_tarefas/tarefas/static/js/scripts.js
@@ -0,0 +1,9 @@
+acertaClasseUsuario = function(){
+    $('input').addClass('form-control');
+}
+
+acertaClasseTarefa = function(){
+    $('input').addClass('form-control');
+    $('textarea').addClass('form-control');
+    $('input[type=checkbox]').removeClass('form-control');
+}
--- minhas_tarefas/tarefas/views/generic.py
+++ minhas_tarefas/tarefas/views/generic.py
@@ -1,14 +1,17 @@
 from django.views.generic import ArchiveIndexView
 from django.views.generic import DetailView
 from tarefas.models import Tarefa
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
 
 
 class TarefasView(ArchiveIndexView):
     model = Tarefa
     date_field = 'data_de_criacao'
 
+    @method_decorator(login_required)
     def get(self, request, *args, **kwargs):
-        self.queryset = Tarefa.objects.filter(usuario=request.user)
+        self.queryset = Tarefa.objects.filter(usuario=request.user.id)
         return super().get(request, *args, **kwargs)
 
 
